e415abf0e8f3571f860d4966872bfd38d73a135c
Frode Ulriksen <frode.ulriksen@outlook.com>
2025-04-16 08:27:01 +0200

    Add buffer shortcuts

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.api.nvim_set_keymap
+for i = 1, 9 do
+  map(
+    "n",
+    "<leader>" .. i .. "",
+    "<Cmd>BufferLineGoToBuffer " .. i .. "<CR>",
+    { noremap = true, silent = true, desc = "Goto Buffer " .. i }
+  )
+end

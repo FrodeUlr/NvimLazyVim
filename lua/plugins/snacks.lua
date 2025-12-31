a2f94350be9560dd756f0e2b12aad1cc5b026cc1
Frode Ulriksen <frode.ulriksen@outlook.com>
2025-12-31 09:48:57 +0100

    add image support

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      image = {
+        enabled = true,
+
+        backend = "wezterm",
+        inline = true,
+        floating = true,
+
+        markdown = {
+          enabled = true,
+        },
+      },
+    },
+  },
+}
